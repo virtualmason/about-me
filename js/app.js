0e05a2e7d9dfc71cbeda1f7362ae710aa515aedf
Floyd Orr <floydcanhelp@gmail.com>
2019-03-05 07:59:56 -0600

    animate header

--- js/app.js
+++ js/app.js
@@ -1,10 +1,14 @@
 
-// ()=> {
-//     setInterval(
 
-
-
-
-  (function () {
-    document.querySelector("header").style.left = "200px";
-  }());
+    
+    window.onload = function(){
+        let pos = 0;
+        let int = setInterval(go, 2000);
+        function go() { 
+             document.querySelector("header").style.left = "320px";  
+             pos++;
+             pos === 200? clearInterval(int): pos++;
+         };
+    
+    }
+    
