578158d5afb67f90827c1154334a96a5ea6029f2
Floyd Orr <floydcanhelp@gmail.com>
2019-03-05 08:09:44 -0600

    add js

--- js/app.js
+++ js/app.js
@@ -5,8 +5,8 @@
         let pos = 0;
         let int = setInterval(go, 2000);
         function go() { 
-             document.querySelector("header").style.left = "320px";  
-             pos++;
+             document.querySelector("header").style.left = 60 + pos +"px";  
+             pos+=  20;
              pos === 200? clearInterval(int): pos++;
          };
     
